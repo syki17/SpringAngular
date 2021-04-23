081c1e54e727c13035bf2326e4a666f4aa9f5a91
Jakub <jakub.syk@gmail.com>
2021-04-22 21:24:24 -0400

    Section 8 - Bootstrap CSS, Conditionals, and Formatting

--- angular-training/sales-project/src/app/sales-person-list/sales-person-list.component.ts
+++ angular-training/sales-project/src/app/sales-person-list/sales-person-list.component.ts
@@ -3,7 +3,7 @@ import { SalesPerson } from './sales-person';
 
 @Component({
   selector: 'app-sales-person-list',
-  templateUrl: './sales-person-list.component.html',
+  templateUrl: './sales-person-list-bootstrap.component.html',
   styleUrls: ['./sales-person-list.component.css']
 })
 export class SalesPersonListComponent implements OnInit {
@@ -11,8 +11,8 @@ export class SalesPersonListComponent implements OnInit {
   //create an array of objects
 salesPersonList: SalesPerson[] = [
   new SalesPerson("Pavel","Zacha","pavel.zacha@njd.com",15000),
-  new SalesPerson("Nico","Hischier","nico.hischier@njd.com",25000),
-  new SalesPerson("Jack","Hughes","jack.hughes@njd.com",50000),
+  new SalesPerson("Nico","Hischier","nico.hischier@njd.com",51000),
+  new SalesPerson("Jack","Hughes","jack.hughes@njd.com",60000),
   new SalesPerson("Jasper","Bratt","jesper.bratt@njd.com",45000)
 ];
 
