0e414c8688a0ef5dc933849d7119af58f2ff1783
Jakub <jakub.syk@gmail.com>
2021-04-26 23:51:00 -0400

    Revert "Section 9 - eCommerce project - Spring Boot Backend"
    This reverts commit a5877a59240058c546d6b02c1f63358922897ab6.

--- ecommerce-project/02-backend/spring-boot-ecommerce/src/main/java/com/jakub/ecommerce/dao/ProductCategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jakub.ecommerce.dao;
-
-import com.jakub.ecommerce.entity.ProductCategory;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category") // usually it will be created by adding an "s" to the end
-public interface ProductCategoryRepository extends JpaRepository<ProductCategory,Long> {
-}
