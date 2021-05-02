6bd20767798b89f94ddb957c4d8b783deac86874
Jakub <jakub.syk@gmail.com>
2021-05-02 14:09:42 -0400

    Section 10 - Angular Front End Product List Added CrossOrigin to backend Created frontend component to pull data from backend

---
+++ ecommerce-project/03-frontend/angular-ecommerce/src/app/common/product.ts
@@ -0,0 +1,12 @@
+export class Product {
+    ski: string;
+    name: string;
+    description: string;
+    unitPrice: number;
+    imageUrl: string;
+    active: boolean;
+    unitsInStock: number;
+    dateCreated: Date;
+    lastUpdated: Date;
+
+}
