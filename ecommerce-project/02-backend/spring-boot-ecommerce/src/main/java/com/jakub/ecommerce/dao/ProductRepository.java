083bd4d03c6bf7c1d9c6816745ca64139a4a8453
Jakub <jakub.syk@gmail.com>
2021-05-31 00:28:39 -0400

    Section 12 eCommerce project -  Search for products by Category (Dynamic Search Component)

--- ecommerce-project/02-backend/spring-boot-ecommerce/src/main/java/com/jakub/ecommerce/dao/ProductRepository.java
+++ ecommerce-project/02-backend/spring-boot-ecommerce/src/main/java/com/jakub/ecommerce/dao/ProductRepository.java
@@ -2,10 +2,14 @@ package com.jakub.ecommerce.dao;
 
 
 import com.jakub.ecommerce.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product,Long> {
-
+    //Query method example below - SELECT * FROM product where category_id=?
+    Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
 }
--- ecommerce-project/03-frontend/angular-ecommerce/src/app/services/product.service.ts
+++ ecommerce-project/03-frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -10,12 +10,16 @@ import { map } from 'rxjs/operators';
 })
 export class ProductService {
 
-  private baseUrl = 'http://localhost:8080/api/products?size=100';
+  private baseUrl = 'http://localhost:8080/api/products';
 
   constructor(private httpClient: HttpClient) { }
 
-  getProductList(): Observable<Product[]> {
-    return this.httpClient.get<GetResponse>(this.baseUrl).pipe(map(response => response._embedded.products))
+  getProductList(theCategoryId: number): Observable<Product[]> {
+
+    //need to build URL based on category id
+    const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`; 
+
+    return this.httpClient.get<GetResponse>(searchUrl).pipe(map(response => response._embedded.products))
   }
 }
 
